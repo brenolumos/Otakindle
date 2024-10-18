944a98937ee31927e627f96b9a02e530f828611f
Bruno Lemos <91765091+brenolumos@users.noreply.github.com>
2024-10-18 14:04:00 -0300

    Add basic structure

--- Otakindle/Form1.Designer.cs
+++ Otakindle/Form1.Designer.cs
@@ -13,13 +13,107 @@
         #region Windows Form Designer generated code
 
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.libraryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.searchToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.createdByGithubbrenolumosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.enterYourKindleEmailToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripTextBox1 = new System.Windows.Forms.ToolStripTextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.libraryToolStripMenuItem,
+            this.searchToolStripMenuItem,
+            this.settingsToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(530, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // libraryToolStripMenuItem
+            // 
+            this.libraryToolStripMenuItem.Name = "libraryToolStripMenuItem";
+            this.libraryToolStripMenuItem.Size = new System.Drawing.Size(55, 20);
+            this.libraryToolStripMenuItem.Text = "Library";
+            this.libraryToolStripMenuItem.Click += new System.EventHandler(this.libraryToolStripMenuItem_Click);
+            // 
+            // searchToolStripMenuItem
+            // 
+            this.searchToolStripMenuItem.Name = "searchToolStripMenuItem";
+            this.searchToolStripMenuItem.Size = new System.Drawing.Size(54, 20);
+            this.searchToolStripMenuItem.Text = "Search";
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.enterYourKindleEmailToolStripMenuItem,
+            this.toolStripTextBox1});
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.settingsToolStripMenuItem.Text = "Settings";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.createdByGithubbrenolumosToolStripMenuItem});
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // createdByGithubbrenolumosToolStripMenuItem
+            // 
+            this.createdByGithubbrenolumosToolStripMenuItem.Name = "createdByGithubbrenolumosToolStripMenuItem";
+            this.createdByGithubbrenolumosToolStripMenuItem.Size = new System.Drawing.Size(239, 22);
+            this.createdByGithubbrenolumosToolStripMenuItem.Text = "created by: github/brenolumos";
+            // 
+            // enterYourKindleEmailToolStripMenuItem
+            // 
+            this.enterYourKindleEmailToolStripMenuItem.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold);
+            this.enterYourKindleEmailToolStripMenuItem.Name = "enterYourKindleEmailToolStripMenuItem";
+            this.enterYourKindleEmailToolStripMenuItem.Size = new System.Drawing.Size(210, 22);
+            this.enterYourKindleEmailToolStripMenuItem.Text = "Enter your Kindle e-mail:";
+            // 
+            // toolStripTextBox1
+            // 
+            this.toolStripTextBox1.BackColor = System.Drawing.SystemColors.Info;
+            this.toolStripTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.toolStripTextBox1.Font = new System.Drawing.Font("Times New Roman", 9F);
+            this.toolStripTextBox1.Name = "toolStripTextBox1";
+            this.toolStripTextBox1.Size = new System.Drawing.Size(100, 21);
+            this.toolStripTextBox1.Click += new System.EventHandler(this.toolStripTextBox1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(530, 642);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem searchToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem libraryToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem createdByGithubbrenolumosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem enterYourKindleEmailToolStripMenuItem;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBox1;
     }
 }
 
--- Otakindle/Form1.cs
+++ Otakindle/Form1.cs
@@ -13,5 +13,29 @@ namespace Otakindle {
         public Form1() {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e) {
+
+        }
+
+        private void toolStripComboBox1_Click(object sender, EventArgs e) {
+
+        }
+
+        private void libraryToolStripMenuItem_Click(object sender, EventArgs e) {
+
+        }
+
+        private void toolStripComboBox1_Click_1(object sender, EventArgs e) {
+
+        }
+
+        private void toolStripTextBox1_Click(object sender, EventArgs e) {
+
+        }
+
+        private void toolStripTextBox1_Click_1(object sender, EventArgs e) {
+            var email = sender.ToString();
+        }
     }
 }
